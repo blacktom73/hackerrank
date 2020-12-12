5189946a61dd45aae8360a2b921e07f5b8f3acd2
TamásMitró <blacktom73@gmail.com>
2020-12-12 18:24:13 +0100

    ADD: BirthdayCandles task - add new tests

--- birthdaycandles/src/test/java/hu/mitro/ResultTest.java
+++ birthdaycandles/src/test/java/hu/mitro/ResultTest.java
@@ -10,28 +10,36 @@ import java.util.List;
 
 class ResultTest {
 
-	private List<Integer> input;
+	private List<Integer> input1;
+	private List<Integer> input2;
 
 	@BeforeEach
 	void setUp() {
-		input = Arrays.asList(3, 1, 2, 3);
+		input1 = Arrays.asList(3, 1, 2, 3);
+		input2 = Arrays.asList(3, 1, 2, 3, 3);
 	}
 
 	@Test
-	void birthdayCakeCandles() {
-		int result = Result.birthdayCakeCandles(input);
+	void birthdayCakeCandles_input1() {
+		int result = Result.birthdayCakeCandles(input1);
 		Assertions.assertEquals(2, result);
 	}
 
 	@Test
+	void birthdayCakeCandles_input2() {
+		int result = Result.birthdayCakeCandles(input2);
+		Assertions.assertEquals(3, result);
+	}
+
+	@Test
 	public void testBirthdayCakeCandles2() {
-		int result = Result.birthdayCakeCandles2(input);
+		int result = Result.birthdayCakeCandles2(input1);
 		Assertions.assertEquals(2, result);
 	}
 
 	@Test
 	public void testBirthdayCakeCandles3() {
-		int result = Result.birthdayCakeCandles3(input);
+		int result = Result.birthdayCakeCandles3(input1);
 		Assertions.assertEquals(2, result);
 	}
 }
