ed2d3b4203566a95f72020f1a587b1c5f758fd0e
TamásMitró <blacktom73@gmail.com>
2020-12-21 22:31:43 +0100

    FIX: TimeConversion task solution failing tests

--- timeconversion/src/main/java/hu/mitro/Solution.java
+++ timeconversion/src/main/java/hu/mitro/Solution.java
@@ -12,20 +12,32 @@ public class Solution {
 	 * Complete the timeConversion function below.
 	 */
 	static String timeConversion(String s) {
+		String[] split;
 		if (s.endsWith("AM")) {
 			if (s.equals("12:00:00AM")) {
 				return "00:00:00";
 			}
-			return s.substring(s.indexOf("AM"));
+			split = getSplitParts(s, "AM");
+			if ((Integer.parseInt(split[0]) == 12 && Integer.parseInt(split[1]) > 0)
+					|| (Integer.parseInt(split[0]) == 12 && Integer.parseInt(split[2]) > 0)) {
+				return "00:" + split[1] + ":" + split[2];
+			}
+			return s.substring(0, s.indexOf("AM"));
 		} else {
 			if (s.equals("12:00:00PM")) {
 				return "12:00:00";
 			}
-			String temp = s.substring(0, s.indexOf("PM"));
-			String[] splitted = temp.split(":");
-			int hours = Integer.parseInt(splitted[0]) + 12;
-			return hours + ":" + splitted[1] + ":" + splitted[2];
+			split = getSplitParts(s, "PM");
+			if (Integer.parseInt(split[0]) == 12) {
+				return split[0] + ":" + split[1] + ":" + split[2];
+			}
+			return Integer.parseInt(split[0]) + 12 + ":" + split[1] + ":" + split[2];
 		}
+	}
+
+	private static String[] getSplitParts(String s, String trail) {
+		String timeParts = s.substring(0, s.indexOf(trail));
+		return timeParts.split(":");
 	}
 
 	private static final Scanner scan = new Scanner(System.in);
